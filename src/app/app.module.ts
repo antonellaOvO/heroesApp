4376fc2cdbb1e090fd31fe3ab1571ced8334f160
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2025-02-14 11:43:47 +0100

    configured HeroesService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SharedModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
